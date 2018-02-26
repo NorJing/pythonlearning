b9a467a955aba0a6910d1fa82e388b1035454c68
Dong Yan <myabdemon@gmail.com>
2018-02-26 16:58:01 +0100

    Update update_dns_file.py

--- code/update_dns_file.py
+++ code/update_dns_file.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python
-# Python 2.7.10
+# Python 2.7.10 & 2.7.5
 
 # update dns zone file serial number when add or update record
 
 import re
+import sys
+
+if (len(sys.argv) < 3):
+    print "Need 3 parameters!. Quit!"
+    quit()
+
+org_file=str(sys.argv[1])
+new_file=str(sys.argv[2])
+print "org_file=%s" % org_file
+print "new_file=%s" % new_file
 
 try:
-    with open('zone.file.2', 'r') as file:
-        with open('zone.file.22','w') as new_file:
+    with open(org_file, 'r') as file:
+        with open(new_file,'w') as new_file:
             for line in file:
-                if "serial" in line: 
-                    # \s+(\d+)\s+;\sserial,\s
-                    the_line = re.match(r'(\s+)(\d+)(\s+;\sserial,\s.*)', line, re.M|re.I)
+                if "serial" in line:
+                    # \s+(\d+)(\s+)?;\sserial\s
+                    the_line = re.match(r'(\s+)(\d+)(\s+)?(;\sserial\s.*)', line, re.M|re.I)
                     if the_line:
                         serial_number = "{0}".format(the_line.group(2))
                         print serial_number
                         print line.rstrip()
                         new_serial_number = int(serial_number) + 1
                         print "new serial number=%s" % new_serial_number
-                        new_line = "{0}{1}{2}".format(the_line.group(1), new_serial_number, the_line.group(3))
-                        print new_line                        
+                        if the_line.group(3) is None:
+                            new_line = "{0}{1}{2}".format(the_line.group(1), new_serial_number, the_line.group(4))
+                        else:
+                            new_line = "{0}{1}{2}{3}".format(the_line.group(1), new_serial_number, the_line.group(3), the_line.group(4))
+                        print new_line
                         # write to new file
-                        new_file.write(str(new_line + '\n'))
+                        new_file.write(str(new_line))
+                    else:
+                        print "no serial line match!"
                 else:
                     new_file.write(str(line))
 except IOError:
